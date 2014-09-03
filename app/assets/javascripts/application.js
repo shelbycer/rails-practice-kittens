6d610eb6380f83c756f60f3e208c52786c448063
Shelby Kelly <shelby.kelly@colorado.edu>
2014-09-03 16:40:00 -0600

    kittens are clickable

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,13 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$(document).ready(function () {
+
+
+  $('.kitten').on('click', function () {
+    window.alert("I don't know how to put a form in javascript");
+  });
+
+});
+
